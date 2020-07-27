ce6194ba4205bbe6c8d4ff6d562c69c5f8a815fd
Cigashamwa byamungu <ebyamungu28@gmail.com>
2020-07-27 15:41:23 +0000

    Everything

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 List1 = ['January', 'Febraury', 'March', 'April', 'May', 'June', 'July', 'August', 'September','October', 'November', 'December']
 
-def function( A, B, C, D):
+L2 = int(input(""))
+
+
+def function( ):
   print("A: Eli")
   print("B: January")
   print("C: 16th")
   print("D: 2004")
 
 function("A:", "B:", "C:", "D:")
+
